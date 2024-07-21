8b3eb790ba8a4a8b6e5e352554eecadf857de3a0
EBRAHIM-CODE <ebrahim0ramadhan@gmail.com>
2024-07-21 15:31:38 +0300

    for now

--- server.js
+++ server.js
@@ -1,25 +1,56 @@
-// Here is where we import modules
-// We begin by loading Express
+//Imported modules
+const dotenv = require("dotenv").config(); 
 const express = require("express");
-const morgan = require('morgan')
+const morgan = require('morgan');
+// const mongoose = require('mongoose'); This is not needed anymore bec i have config folder
 
+// dotenv.config(); This not needed
 const app = express();
-app.use(morgan('dev'))
 
-// server.js
+//Database call
+require('./config/database.js');
 
-// GET /
-app.get("/", async (req, res) => {
+// We're importing it here because this file has the route handlers
+const Fruit = require("./models/fruit.js");
+app.use(express.urlencoded({ extended: false }));
+
+
+//Middleware
+app.use(morgan('dev'));
+
+
+
+//Routes
+
+//Landing Page
+app.get("/", (req, res, next) => {
     res.render("index.ejs");
   });
 
 
+  app.get("/fruits/new", (req, res) => {
+    res.render('fruits/new.ejs');
+  });
 
+  // server.js
 
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    res.redirect("/fruits/new");
+  });
 
-
+  // GET /fruits
+  app.get("/fruits", async (req, res) => {
+    const allFruits = await Fruit.find();
+    res.render("fruits/index.ejs", { fruits: allFruits });
+  });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
-  
 });
