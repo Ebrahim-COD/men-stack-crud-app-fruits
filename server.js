3f65d508cf7bfa5e84a860f50b1e51fcc09c11bb
EBRAHIM-CODE <ebrahim0ramadhan@gmail.com>
2024-07-21 16:55:04 +0300

    finished lecture

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Imported modules
 const dotenv = require("dotenv").config(); 
 const express = require("express");
+const methodOverride = require("method-override"); // new
 const morgan = require('morgan');
 // const mongoose = require('mongoose'); This is not needed anymore bec i have config folder
 
@@ -13,6 +14,9 @@ require('./config/database.js');
 // We're importing it here because this file has the route handlers
 const Fruit = require("./models/fruit.js");
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); // new
+app.use(morgan("dev")); //new
+
 
 
 //Middleware
@@ -49,6 +53,40 @@ app.post("/fruits", async (req, res) => {
   app.get("/fruits", async (req, res) => {
     const allFruits = await Fruit.find();
     res.render("fruits/index.ejs", { fruits: allFruits });
+  });
+
+  app.get("/fruits/:fruitId", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/show.ejs", { fruit: foundFruit });
+  });
+
+  app.delete("/fruits/:fruitId", async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
+  });
+
+  app.get("/fruits/:fruitId/edit", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/edit.ejs", {
+      fruit: foundFruit,
+    });
+  });
+
+  // server.js
+
+app.put("/fruits/:fruitId", async (req, res) => {
+    // Handle the 'isReadyToEat' checkbox data
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    
+    // Update the fruit in the database
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+  
+    // Redirect to the fruit's show page to see the updates
+    res.redirect(`/fruits/${req.params.fruitId}`);
   });
 
 app.listen(3000, () => {
