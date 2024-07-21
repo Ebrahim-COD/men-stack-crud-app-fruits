a0f1becb8e4b8b1612ffebdba15ee0175db3ac91
EBRAHIM-CODE <ebrahim0ramadhan@gmail.com>
2024-07-21 12:34:25 +0300

    templating

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 // Here is where we import modules
 // We begin by loading Express
 const express = require("express");
+const morgan = require('morgan')
 
 const app = express();
+app.use(morgan('dev'))
+
+// server.js
+
+// GET /
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+
+
+
+
+
+
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
+  
 });
